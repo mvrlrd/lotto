c9188e8db17323f41eec0e31fff5592c48c26a60
mvrlrd <mvrlrd@ya.ru>
2022-10-14 17:18:58 +0300

    refactored

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,106 +1,109 @@
 
 
-    import kotlin.random.Random
-    class Lotto {
+import kotlin.random.Random
 
-        val players = mutableListOf<Person>() // определите поле, в котром будут храниться добавленные игроки `Person`
-        // поле thrownNumbers должно хранить в себе набор выброшенных чисел.
-        val thrownNumbers: HashSet<Int> = hashSetOf() // определите подходящую структуру данных
+class Lotto {
+    val players = mutableListOf<Person>() // определите поле, в котром будут храниться добавленные игроки `Person`
+    val thrownNumbers: HashSet<Int> = hashSetOf() // определите подходящую структуру данных
 
-        fun addPerson(person: Person) {
-            players.add(person) // добавить игрока в список игроков
-        }
-
-        fun start() {
-            // вывести сообщение "Перед началом игры необходимо добавить хотя бы двух игроков" и завершить работу, если количество добавленных игроков меньше 2
-            val lotto = Lotto()
-//        lotto.addPerson(Person("a"))
-//        lotto.addPerson(Person("b"))
-            if (lotto.players.size<2){
-                println("Перед началом игры необходимо добавить хотя бы двух игроков")
-                return
-            }
-
-            while (true) {
-                val winners = mutableListOf<Person>()
-                val num = Random.nextInt(
-                    1,
-                    99
-                )
-                println("выброшенное число $num")
-                // достать номер. Номер может быть в диапазоне от 1 до 99 включительно
-                if (!lotto.thrownNumbers.contains(num)) {
-                    lotto.thrownNumbers.add(num)
-                    for (player in lotto.players) {
-                        if (player.card.numbers.values.any { it.contains(num) }) {
-                            val pair = player.card.numbers.filter { it.value.contains(num) }
-                            val line = pair.keys.first()
-                            player.card.numbers[line]?.remove(num)
-                            if (player.card.numbers[line]!!.isEmpty()) {
-                                winners.add(player)
-                            }
-                        }
-                    }
-                    if (winners.isNotEmpty()){
-                        winners.forEach { player->println("Победитель: ${player.name}!!!") }
-                        return
-                    }
-                }
-            }
-        }
-        // после каждого выброшенного числа удалять это число из карточек всех игроков, если такое число имеется
-        // выбрасывать новые числа до тех пор, пока не определится победитель
-        // побеждает тот, у кого в одном из рядов нет больше чисел. Победителей может быть более одного
-        // после того как появляется победитель, для каждого победителя вывести текст "Победитель: [имя_победителя]!!!"
+    fun addPerson(person: Person) {
+        players.add(person) // добавить игрока в список игроков
     }
 
-        class Card(val numbers: Map<Int, MutableSet<Int>>)
-
-        class Person(val name: String) {
-
-            val card: Card = createCard()
-
-            private fun createCard(): Card {
-                val numbers: Set<Int> = generateNumbers()
-
-                val iterator: Iterator<Int> = numbers.iterator()
-                var currentLine = 1
-
-                val cardNumbers: MutableMap<Int, MutableSet<Int>> = mutableMapOf(
-                    1 to mutableSetOf(),
-                    2 to mutableSetOf(),
-                    3 to mutableSetOf()
-                )
-
-                while (iterator.hasNext()) {
-                    val number = iterator.next()
-                    cardNumbers[currentLine]?.add(number)
-
-                    if (currentLine < 3) {
-                        currentLine++
-                    } else {
-                        currentLine = 1
+    fun start() {
+        if (players.size < 2) {
+            println("Перед началом игры необходимо добавить хотя бы двух игроков")
+            return
+        }
+        players.forEach { println(it.card.numbers.values.toString()) }
+        while (true) {
+            val winners = mutableListOf<Person>()
+            val num = Random.nextInt(
+                1,
+                100
+            )
+            if (thrownNumbers.contains(num)) {
+                continue
+            } else {
+                println("выброшенное число $num")
+                thrownNumbers.add(num)
+                for (player in players) {
+                    if (player.card.numbers.values.any { it.contains(num) }) {
+                        val pair = player.card.numbers.filter { it.value.contains(num) }
+                        val line = pair.keys.first()
+                        player.card.numbers[line]?.remove(num)
+                        if (player.card.numbers[line]!!.isEmpty()) {
+                            winners.add(player)
+                        }
                     }
                 }
-
-                return Card(cardNumbers)
-            }
-
-            private fun generateNumbers(): Set<Int> {
-                val numbers: MutableSet<Int> = mutableSetOf()
-
-                while (numbers.size < NUMBERS_COUNT) {
-                    numbers.add(Random.nextInt(MIN_NUMBER, MAX_NUMBER))
+                if (winners.isNotEmpty()) {
+                    winners.forEach { player -> println("Победитель: ${player.name}!!!") }
+               //     players.forEach { println(it.card.numbers.values.toString()) }
+                    return
                 }
-
-                return numbers
             }
 
-            private companion object {
+        }
+    }
+}
 
-                private const val NUMBERS_COUNT = 15
-                private const val MAX_NUMBER = 100
-                private const val MIN_NUMBER = 1
+class Card(val numbers: Map<Int, MutableSet<Int>>)
+
+class Person(val name: String) {
+
+    val card: Card = createCard()
+
+    private fun createCard(): Card {
+        val numbers: Set<Int> = generateNumbers()
+
+        val iterator: Iterator<Int> = numbers.iterator()
+        var currentLine = 1
+
+        val cardNumbers: MutableMap<Int, MutableSet<Int>> = mutableMapOf(
+            1 to mutableSetOf(),
+            2 to mutableSetOf(),
+            3 to mutableSetOf()
+        )
+
+        while (iterator.hasNext()) {
+            val number = iterator.next()
+            cardNumbers[currentLine]?.add(number)
+
+            if (currentLine < 3) {
+                currentLine++
+            } else {
+                currentLine = 1
             }
         }
+
+        return Card(cardNumbers)
+    }
+
+    private fun generateNumbers(): Set<Int> {
+        val numbers: MutableSet<Int> = mutableSetOf()
+
+        while (numbers.size < NUMBERS_COUNT) {
+            numbers.add(Random.nextInt(MIN_NUMBER, MAX_NUMBER))
+        }
+
+        return numbers
+    }
+
+    private companion object {
+
+        private const val NUMBERS_COUNT = 15
+        private const val MAX_NUMBER = 100
+        private const val MIN_NUMBER = 1
+    }
+}
+
+//fun main(){
+//    val lotto = Lotto()
+//    lotto.addPerson(Person("a"))
+//    lotto.addPerson(Person("b"))
+//    lotto.addPerson(Person("c"))
+//    lotto.addPerson(Person("d"))
+//    lotto.start()
+//}
 
