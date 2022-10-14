476283d9fa427a90d638b8db25e1814d45d8d748
mvrlrd <mvrlrd@ya.ru>
2022-10-14 16:19:20 +0300

    LottoKt done

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,26 +1,57 @@
 
 
     import kotlin.random.Random
-
     class Lotto {
 
-        // определите поле, в котром будут храниться добавленные игроки `Person`
+        val players = mutableListOf<Person>() // определите поле, в котром будут храниться добавленные игроки `Person`
         // поле thrownNumbers должно хранить в себе набор выброшенных чисел.
-        val thrownNumbers:  // определите подходящую структуру данных
+        val thrownNumbers: HashSet<Int> = hashSetOf() // определите подходящую структуру данных
 
         fun addPerson(person: Person) {
-            // добавить игрока в список игроков
+            players.add(person) // добавить игрока в список игроков
         }
 
         fun start() {
             // вывести сообщение "Перед началом игры необходимо добавить хотя бы двух игроков" и завершить работу, если количество добавленных игроков меньше 2
+            val lotto = Lotto()
+//        lotto.addPerson(Person("a"))
+//        lotto.addPerson(Person("b"))
+            if (lotto.players.size<2){
+                println("Перед началом игры необходимо добавить хотя бы двух игроков")
+                return
+            }
 
-            // достать номер. Номер может быть в диапазоне от 1 до 99 включительно
-            // после каждого выброшенного числа удалять это число из карточек всех игроков, если такое число имеется
-            // выбрасывать новые числа до тех пор, пока не определится победитель
-            // побеждает тот, у кого в одном из рядов нет больше чисел. Победителей может быть более одного
-            // после того как появляется победитель, для каждого победителя вывести текст "Победитель: [имя_победителя]!!!"
+            while (true) {
+                val winners = mutableListOf<Person>()
+                val num = Random.nextInt(
+                    1,
+                    99
+                )
+                println("выброшенное число $num")
+                // достать номер. Номер может быть в диапазоне от 1 до 99 включительно
+                if (!lotto.thrownNumbers.contains(num)) {
+                    lotto.thrownNumbers.add(num)
+                    for (player in lotto.players) {
+                        if (player.card.numbers.values.any { it.contains(num) }) {
+                            val pair = player.card.numbers.filter { it.value.contains(num) }
+                            val line = pair.keys.first()
+                            player.card.numbers[line]?.remove(num)
+                            if (player.card.numbers[line]!!.isEmpty()) {
+                                winners.add(player)
+                            }
+                        }
+                    }
+                    if (winners.isNotEmpty()){
+                        winners.forEach { player->println("Победитель: ${player.name}!!!") }
+                        return
+                    }
+                }
+            }
         }
+        // после каждого выброшенного числа удалять это число из карточек всех игроков, если такое число имеется
+        // выбрасывать новые числа до тех пор, пока не определится победитель
+        // побеждает тот, у кого в одном из рядов нет больше чисел. Победителей может быть более одного
+        // после того как появляется победитель, для каждого победителя вывести текст "Победитель: [имя_победителя]!!!"
     }
 
         class Card(val numbers: Map<Int, MutableSet<Int>>)
